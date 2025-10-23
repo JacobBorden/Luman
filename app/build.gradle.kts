c478016f341b21a644be85dfddbc1942bcafd597
Jacob N Borden <amenra.beats@gmail.com>
2025-10-23 09:44:23 -0300

    Raise minSdk and clean manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.lumen"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "0.1.0"
